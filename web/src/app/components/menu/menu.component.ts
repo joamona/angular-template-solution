eba4846664cdd58efa523a6bdd6557c12ca07bbe
joamona <joamona@cgf.upv.es>
2025-04-11 13:03:57 +0200

    m

--- web/src/app/components/menu/menu.component.ts
+++ web/src/app/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
