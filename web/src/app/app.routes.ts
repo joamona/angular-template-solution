620c3895cc019b4ffd028695439e4d32673e0ad6
joamona <joamona@cgf.upv.es>
2025-04-29 14:38:53 +0200

    m

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MapComponent } from './components/map/map.component';
+import { AboutComponent } from './components/about/about.component';
+import { HelpComponent } from './components/help/help.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: 'help', component:HelpComponent},
+    {path: 'about', component:AboutComponent},
+    {path: 'map', component:MapComponent},
+];
--- web/src/app/components/menu/menu.component.ts
+++ web/src/app/components/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
